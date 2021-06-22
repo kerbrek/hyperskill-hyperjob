67aa48c98db57d0857061cee8cc05fbf7bc9a727
kerbrek <kerbrek@gmail.com>
2021-06-22 18:13:41 +0200

    disable DEBUG by default in production.py

--- hyperjob/production.py
+++ hyperjob/production.py
@@ -7,7 +7,7 @@ from .settings import *
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
