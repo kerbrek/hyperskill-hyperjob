38633d703e5ab31562e95f689364460708bbfd58
kerbrek <kerbrek@gmail.com>
2021-06-29 22:04:19 +0200

    add Sentry configuration to production.py settings

--- hyperjob/production.py
+++ hyperjob/production.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .settings import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
@@ -51,3 +53,20 @@ DATABASES = {
         'PASSWORD': os.environ['DBPASS']
     }
 }
+
+
+# Sentry configuration
+if 'SENTRY_DSN' in os.environ:
+    sentry_sdk.init(
+        dsn=os.environ['SENTRY_DSN'],
+        integrations=[DjangoIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=float(os.environ.get('SENTRY_TRACES_SAMPLE_RATE', default=1.0)),
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=int(os.environ.get('SENTRY_SEND_DEFAULT_PII', default=1))
+    )
