087e1ad9738240612da8dee3eb744d416905ba50
kerbrek <kerbrek@gmail.com>
2021-06-30 18:51:35 +0200

    add logging cals

--- vacancy/views.py
+++ vacancy/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect, render
@@ -5,6 +7,15 @@ from django.views import View
 
 from .forms import NewVacancyForm
 from .models import Vacancy
+
+logger = logging.getLogger(__name__)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        return x_forwarded_for.split(',')[0]
+    return request.META.get('REMOTE_ADDR')
 
 
 # Create your views here.
@@ -18,10 +29,18 @@ class NewVacancyView(View):
     def post(self, request, *args, **kwargs):
         can_submit = request.user.is_authenticated and request.user.is_staff
         if not can_submit:
+            username = request.user.username
+            ip = get_client_ip(request)
+            logger.warning(
+                'User "%s" with IP %s can not submit NewVacancy', username, ip)
             raise PermissionDenied
 
         form = NewVacancyForm(request.POST)
         if not form.is_valid():
+            username = request.user.username
+            ip = get_client_ip(request)
+            logger.warning(
+                'User "%s" with IP %s submitted non-valid NewVacancy', username, ip)
             return HttpResponseBadRequest('<h1>400 Bad Request</h1>')
 
         author = request.user
