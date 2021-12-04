f0b49a393aece04b216ee41b559cc3ee408b817a
kerbrek <kerbrek@gmail.com>
2021-12-04 21:31:12 +0200

    change logging settings in gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,6 @@ import os
 WORKERS_DEFAULT = multiprocessing.cpu_count() * 2 + 1
 
 workers = int(os.environ.get("WORKERS", WORKERS_DEFAULT))
+errorlog = "-"
+accesslog = "-"
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
